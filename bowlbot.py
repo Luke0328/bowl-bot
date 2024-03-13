66e601027c572de2de21a4e7a218c6390b0c1c5a
Luke0328 <curly0328@gmail.com>
2024-03-13 16:57:05 -0600

    Addscore implementation, other stats implemented

--- bowlbot.py
+++ bowlbot.py
@@ -1,9 +1,11 @@
+import datetime
 from db import Database
 from discord.ext import commands
 
 class BowlBot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.db = Database()
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -17,12 +19,58 @@ class BowlBot(commands.Cog):
         ])
         await ctx.send(help_msg)
 
+    # addscore pts strikes spares extra_open_frame
     @commands.command()
     async def addscore(self, ctx):
         author = ctx.author
 
+        await ctx.send('Please enter your score in the following format: \'points strikes spares\'')
+        
+        def check(m):
+            return m.author == author and m.channel == ctx.channel
+        
+        score_msg = await self.bot.wait_for('message', check=check)
+        print(score_msg.content)
 
-        await ctx.send('Scores added!')
+        await ctx.send('Did you receive an extra frame? (y/n)')
+        extra_frame_msg = await self.bot.wait_for('message', check=check)
+        print(extra_frame_msg.content)
+
+        score_data = score_msg.content.split()
+        extra_frame_msg_text = extra_frame_msg.content
+
+        # check score and extra frame messages for errors
+        res_msg = ''
+        if len(score_data) != 3:
+            res_msg = 'Please follow the format provided'
+        elif int(score_data[0]) < 0 or int(score_data[1]) < 0 or int(score_data[2]) < 0:
+            res_msg = 'Points, strikes, or spares can not be negative'
+        elif int(score_data[0]) > 300:
+            res_msg = 'Can not score over 300 points'
+        elif extra_frame_msg_text != 'y' and extra_frame_msg_text != 'n':
+            res_msg = "Please enter only y or n"
+        else:
+            extra = False
+            if (res_msg == 'y'):
+                extra = True
+
+            score = {
+                "pts": int(score_data[0]),
+                "strk": int(score_data[1]),
+                "spr": int(score_data[2]),
+                "extra": extra,
+                "date": datetime.datetime.now(tz=datetime.timezone.utc),
+            }
+            print(score)
+            res_msg = 'Score added!'
+
+        await ctx.send(res_msg)
+
+    @commands.command()
+    async def stats(self, ctx):
+        author = ctx.author
+        stats_msg = self.db.get_stats(author)
+        await ctx.send(stats_msg)
 
 async def setup(client):
     await client.add_cog(BowlBot(client))
--- db.py
+++ db.py
@@ -7,7 +7,7 @@ load_dotenv()
 DB_PWD = os.getenv('DB_PWD')
 URI = 'mongodb+srv://luke0328:{0}@cluster0.45uf3t7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'.format(DB_PWD)
 
-class Database:
+class Database():
     """class to interact with the database"""
     def __init__(self):
         self.client = pymongo.MongoClient(URI)
@@ -20,39 +20,35 @@ class Database:
             print(e)
 
     def add_score(self, player_name, score):
-        # new_score = {
-        #     'pts': 0,
-        #     'strk': 0,
-        #     'spr': 0,
-        #     "date": datetime.datetime.now(tz=datetime.timezone.utc),
-        # }
         # create document if player does not exists
         if (self.players.find_one({'name': player_name}) == None):
             new_player = {
                 'name': player_name,
                 'lft_games': 0,
-                # 'mon_games': 0,
+                'tot_frames': 0,
                 'lft_avg': 0,
-                # 'mon_avg': 0,
-                'lft_strk_pct': 0,
-                # 'mon_strk_pct': 0,                
-                'lft_spr_pct': 0,
-                # 'mon_spr_pct': 0,
+                'lft_strk_r': 0,
+                'lft_conv_r': 0,
                 'scores': [],
             }
             player_id = self.players.insert_one(new_player)
 
+        # get player with name
         player = self.players.find_one({'name': player_name})
-        print(player['lft_games'])
-
+        # calculate new values
         new_lft_games = player['lft_games'] + 1
-        # new_mon_games = player.mon_games + 1
         new_lft_avg = (player['lft_avg'] * player['lft_games'] + score['pts']) / new_lft_games
-        # new_mon_avg = (player.mon_avg * player.mon_games + score.pts) / new_mon_games
+        new_tot_frames = player['tot_frames'] + 10
+        if (score['extra']):
+            new_tot_frames += 1
+        new_lft_strk_r = (player['lft_strk_r'] * player['tot_frames'] + score['strk']) / new_tot_frames
+        new_lft_conv_r = (player['lft_conv_r'] * player['tot_frames'] + score['conv']) / new_tot_frames
 
         res = self.players.update_one({'name': player_name}, {'$set': {
             'lft_games': new_lft_games,
             'lft_avg': new_lft_avg,
+            'lft_strk_r': new_lft_strk_r,
+            'lft_conv_r': new_lft_conv_r,
         }})
 
     def get_stats(self, player_name):
@@ -60,6 +56,13 @@ class Database:
         msg = """
         Player : {0}
         Lifetime Average Score: {1}
-        """.format(player_name, player['lft_avg'])
+        Lifetime Strike Percentage: {2}
+        Lifetime Closed Frame Percentage: {3}
+        """.format(
+            player_name, 
+            player['lft_avg'], 
+            round(player['lft_strk_r'] * 100.0, 1), 
+            round(player['lft_conv_r'] * 100.0, 1)
+        )
         return msg
     
